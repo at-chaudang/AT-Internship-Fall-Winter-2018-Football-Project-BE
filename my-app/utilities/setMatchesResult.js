aba88a21e041f02b91e8906d666396372f4a2164
at-chaudang <chau.dang@asiantech.vn>
2019-03-28 16:35:54 +0700

    Abc

--- my-app/utilities/setMatchesResult.js
+++ my-app/utilities/setMatchesResult.js
@@ -4,13 +4,15 @@ const calcScore = require('../utilities/calcScore');
 const sortKindOfMatches = require('../utilities/sortKindOfMatches');
 const sortByGroup = require('../utilities/sortByGroup');
 
-module.exports = function (scores) {
+module.exports = function (scores, groupName) {
   let { scoresOfAllTables, scoresOfAllQuaterFinal, scoresOfAllSemiFinal, scoresOfAllFinal, scoresOfAllFinal32 } = sortKindOfMatches(scores);
 
   // Nếu các trận bán kết hay tứ kết (với 32 đội) đã được set thì mới bắt đầu set chung kết hay bán kết (với 32 đội)
   let _unSetSemiFinal = scoresOfAllSemiFinal.filter(score => (score.score === null));
   let _unSetFinal = scoresOfAllFinal.filter(score => (score.score === null));
-  if (!_unSetSemiFinal.length) {
+  // if (!_unSetSemiFinal.length) {
+  
+  if (1) {
     let scoresBySemiFinal = sortByGroup(scoresOfAllSemiFinal);
     scoresBySemiFinal.map((_scoresEachGroup, index) => {
       let teamInformation = getTopTeams(_scoresEachGroup, 2)[0];
@@ -22,7 +24,7 @@ module.exports = function (scores) {
 
   let unSetQuarterFinal = scoresOfAllQuaterFinal.filter(score => (score.score === null));
   // Nếu các trận tứ kết hay knockout (với 32 đội) đã được set thì bắt đầu set bán kết hay tứ kết (với 32 đội)
-  if (!unSetQuarterFinal.length) {
+  if (1) {
     let indexsRunning = [0, 1, 2, 3, 4, 5, 6, 7];
     let indexRun = 0;
     let scoresByQuaterFinal = sortByGroup(scoresOfAllQuaterFinal, false);
@@ -38,25 +40,25 @@ module.exports = function (scores) {
   }
 
   // Nếu các trận vòng bảng đã được set thì bắt đầu set tứ kết hay knockout (với 32 đội)
-  let unSetAllKnockOut = scores.filter(score => (score.score === null && score.match_id.round === 1));
-  if (!unSetAllKnockOut.length) {
-    let indexsRunning = [0, 3, 2, 1, 4, 7, 5, 6, 8, 11, 10, 9, 12, 15, 13, 14];
-    let indexRun = 0;
-    let scoresByGroupName = sortByGroup(scoresOfAllTables, false);
-    scoresByGroupName.map((_scoresEachGroup) => {
-      let teamsInformationOfTwelve = calcScore(_scoresEachGroup);
-      let teamsInformation = getTopTeams(teamsInformationOfTwelve, 0);
-      teamsInformation.map(teamInformation => {
-        let indexsRunnings = indexsRunning[indexRun++];
-        let score = new Score(scoresOfAllQuaterFinal[indexsRunnings]);
-        score.tournament_team_id = teamInformation.tournamentTeamId;
-        score.save(err => { if (err) throw err });
-      })
-    });
-  }
+  // let unSetAllKnockOut = scores.filter(score => (score.score === null && score.match_id.round === 1));
+  // if (!unSetAllKnockOut.length) {
+  //   let indexsRunning = [0, 3, 2, 1, 4, 7, 5, 6, 8, 11, 10, 9, 12, 15, 13, 14];
+  //   let indexRun = 0;
+  //   let scoresByGroupName = sortByGroup(scoresOfAllTables, false);
+  //   scoresByGroupName.map((_scoresEachGroup) => {
+  //     let teamsInformationOfTwelve = calcScore(_scoresEachGroup);
+  //     let teamsInformation = getTopTeams(teamsInformationOfTwelve, 0);
+  //     teamsInformation.map(teamInformation => {
+  //       let indexsRunnings = indexsRunning[indexRun++];
+  //       let score = new Score(scoresOfAllQuaterFinal[indexsRunnings]);
+  //       score.tournament_team_id = teamInformation.tournamentTeamId;
+  //       score.save(err => { if (err) throw err });
+  //     })
+  //   });
+  // }
 
   // Nếu các trận bán kết (với 32 đội) đã được set thì bắt đầu set chung kết.
-  if (!_unSetFinal.length) {
+  if (1) {
     let scoresByFinal = sortByGroup(scoresOfAllFinal);
     scoresByFinal.map((_scoresEachGroup, index) => {
       let teamInformation = getTopTeams(_scoresEachGroup, 2)[0];
